2661e1d5cbbd6157f75087eda608996d0b53bc11
iosdev <iosdev@mtk-idev-mba-06.local>
2016-05-01 18:09:41 +0300

    test

--- SwiftProject/MapViewController.swift
+++ SwiftProject/MapViewController.swift
@@ -11,96 +11,30 @@ import CoreData
 
 class MapViewController: UIViewController, EILIndoorLocationManagerDelegate {
     
+    //MARK: Outlets
     @IBOutlet weak var status: UILabel!
     @IBOutlet weak var loading: UIActivityIndicatorView!
     @IBOutlet weak var locationView: EILIndoorLocationView!
+    @IBOutlet weak var parkUnpark: UIButton!
+    
+    //MARK: Variables
     let locationManager = EILIndoorLocationManager()
-    
     var location: EILLocation!
-    var avatar: EILPositionView!
-    var currentPosition : EILOrientedPoint!
-    var navigationLayer: CAShapeLayer!
-    var corePoints: [EILOrientedPoint] = []
-    var spotList: [Spot]!
+    var currentPosition : EILOrientedPoint?
     var selectedSpot: Spot?
+    var userCar: Spot?
+    var isFindingCar: Bool?
+    var navigationLayer: CAShapeLayer!
+    var appDelegate: AppDelegate!
+    var managedContext: NSManagedObjectContext!
     
-    
-    
-    @IBAction func parkButton(sender: UIButton) {
-//        let path = UIBezierPath()
-//        let point1 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 0, y: 4.5))
-//        let point2 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 9, y: 4.5))
-//        path.moveToPoint(point1)
-//        path.addLineToPoint(point2)
-//        
-//        let point3 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 4.5, y: 0))
-//        let point4 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 4.5, y: 9))
-//        path.moveToPoint(point3)
-//        path.addLineToPoint(point4)
-//        
-//        let point5 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 0, y: 2))
-//        let point6 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 9, y: 2))
-//        path.moveToPoint(point5)
-//        path.addLineToPoint(point6)
-//        
-//        let point7 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 0, y: 7))
-//        let point8 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 9, y: 7))
-//        path.moveToPoint(point7)
-//        path.addLineToPoint(point8)
-//        
-//        let point9 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 2, y: 0))
-//        let point10 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 2, y: 9))
-//        path.moveToPoint(point9)
-//        path.addLineToPoint(point10)
-//        
-//        let point11 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 7, y: 0))
-//        let point12 = locationView.calculatePicturePointFromRealPoint(EILOrientedPoint(x: 7, y: 9))
-//        path.moveToPoint(point11)
-//        path.addLineToPoint(point12)
-//        
-//        print("meow")
-//        let navigationLayer1 = CAShapeLayer()
-//        navigationLayer1.path = path.CGPath
-//        navigationLayer1.strokeColor = UIColor.blackColor().CGColor
-//        navigationLayer1.fillColor = UIColor.clearColor().CGColor
-//        navigationLayer1.lineDashPattern = [3,4]
-//        locationView.layer.insertSublayer(navigationLayer1, atIndex: 0)
-        if let selectedSpot = self.selectedSpot {
-            selectedSpot.changeStatus()
-            WebService.sharedInstance.changeStatus(selectedSpot, completionHandler: {(result) -> Void in
-                    let appDelegate =
-                        UIApplication.sharedApplication().delegate as! AppDelegate
-                    
-                    let managedContext = appDelegate.managedObjectContext
-                    
-                    //2
-                    let entity =  NSEntityDescription.entityForName("Spot",
-                        inManagedObjectContext:managedContext)
-                    
-                    let savedSpot = NSManagedObject(entity: entity!,
-                        insertIntoManagedObjectContext: managedContext)
-                    
-                    //3
-                    savedSpot.setValue(selectedSpot.getId(), forKey: "id")
-                savedSpot.setValue(selectedSpot.getX(), forKey: "x")
-                savedSpot.setValue(selectedSpot.getY(), forKey: "y")
-                savedSpot.setValue(selectedSpot.getStatus(), forKey: "status")
-                    
-                    //4
-                    do {
-                        try managedContext.save()
-                    } catch let error as NSError  {
-                        print("Could not save \(error), \(error.userInfo)")
-                    }
-                
-            })
-        }
-    }
-    
+    //Test method
     func test(sender: UITapGestureRecognizer) {
         let currentPoint = locationView.calculateRealPointFromPicturePoint(sender.locationInView(locationView))
         self.currentPosition = EILOrientedPoint(x: currentPoint.x, y: currentPoint.y)
-        updateNavigation()
+        if let currentPosition = self.currentPosition, selectedSpot = self.selectedSpot {
+            updateNavigation(currentPosition, toPoint: selectedSpot.getCoordinate())
+        }
         
         let appDelegate =
             UIApplication.sharedApplication().delegate as! AppDelegate
@@ -108,60 +42,60 @@ class MapViewController: UIViewController, EILIndoorLocationManagerDelegate {
         let managedContext = appDelegate.managedObjectContext
         
         //2
-        let fetchRequest = NSFetchRequest(entityName: "Spot")
+        let fetchRequest = NSFetchRequest(entityName: "UserCar")
         
         //3
         do {
             let results =
                 try managedContext.executeFetchRequest(fetchRequest)
             let savedSpot = results as! [NSManagedObject]
+            print(savedSpot.count)
+            print(self.isFindingCar)
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
         }
     }
     
+    //ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        corePoints.append(EILOrientedPoint(x: 2, y: 2))
-        corePoints.append(EILOrientedPoint(x: 4.5, y: 2))
-        corePoints.append(EILOrientedPoint(x: 7, y: 2))
-        corePoints.append(EILOrientedPoint(x: 2, y: 4.5))
-        corePoints.append(EILOrientedPoint(x: 4.5, y: 4.5))
-        corePoints.append(EILOrientedPoint(x: 7, y: 4.5))
-        corePoints.append(EILOrientedPoint(x: 2, y: 7))
-        corePoints.append(EILOrientedPoint(x: 4.5, y: 7))
-        corePoints.append(EILOrientedPoint(x: 7, y: 7))
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        managedContext = appDelegate.managedObjectContext
         
         let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("test:"))
         locationView.userInteractionEnabled = true
         locationView.addGestureRecognizer(tapGestureRecognizer)
-        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+                
+        //Loading indicator
         view.addSubview(loading)
         loading.startAnimating()
         UIApplication.sharedApplication().beginIgnoringInteractionEvents()
         
         self.locationManager.delegate = self
         ESTConfig.setupAppID("chuongtruong290893-gmail-c-af3", andAppToken: "ce9c0eea8821ff51e583c03310f7ac89")
-        
-        // You will find the identifier on https://cloud.estimote.com/#/locations
         let fetchLocationRequest = EILRequestFetchLocation(locationIdentifier: "classroom-b305-ver2-ajz")
         fetchLocationRequest.sendRequestWithCompletion { (location, error) in
             if let location = location {
                 self.location = location
-                
+                //Set up the locationView
                 self.locationView.showTrace = false
                 self.locationView.rotateOnPositionUpdate = true
                 self.locationView.locationBorderColor = UIColor.purpleColor()
                 self.locationView.locationBorderThickness = 4
                 self.locationView.windowColor = UIColor.yellowColor()
                 self.locationView.drawLocation(location)
-                
+                //Update current position
                 self.locationManager.startPositionUpdatesForLocation(self.location)
+                //Get spots information from the server and draw them in the map
                 WebService.sharedInstance.getSpots({ (result) -> Void in
-                    self.spotList = result
-                    for spot in self.spotList {
+                    for spot in result {
                         self.drawSpot(spot)
                     }
+                    //Hide loading indicator
                     self.loading.removeFromSuperview()
                     UIApplication.sharedApplication().endIgnoringInteractionEvents()
                 })
@@ -169,10 +103,68 @@ class MapViewController: UIViewController, EILIndoorLocationManagerDelegate {
                 print("can't fetch location: \(error)")
             }
         }
-        
-        
+//        if let userCar = self.userCar {
+//            parkUnpark.setTitle("UNPARK", forState: UIControlState.Normal)
+//            parkUnpark.addTarget(self, action: #selector(MapViewController.unPark), forControlEvents: UIControlEvents.TouchUpInside)
+//        } else {
+//            parkUnpark.setTitle("PARK", forState: UIControlState.Normal)
+//            parkUnpark.addTarget(self, action: #selector(MapViewController.park), forControlEvents: UIControlEvents.TouchUpInside)
+//        }
     }
     
+    func park() {
+        if let selectedSpot = self.selectedSpot {
+            selectedSpot.changeStatus()
+            WebService.sharedInstance.changeStatus(selectedSpot, completionHandler: {(result) -> Void in
+                let entity =  NSEntityDescription.entityForName("UserCar",
+                    inManagedObjectContext:self.managedContext)
+                
+                let savedSpot = NSManagedObject(entity: entity!,
+                    insertIntoManagedObjectContext: self.managedContext)
+                
+                //3
+                savedSpot.setValue(selectedSpot.getId(), forKey: "id")
+                savedSpot.setValue(selectedSpot.getX(), forKey: "x")
+                savedSpot.setValue(selectedSpot.getY(), forKey: "y")
+                savedSpot.setValue(selectedSpot.getStatus(), forKey: "status")
+                
+                //4
+                do {
+                    try self.managedContext.save()
+                    let alert = UIAlertView(title: "Alert", message: "Done", delegate: nil, cancelButtonTitle: "Close")
+                    alert.show()
+                    self.parkUnpark.enabled = false
+                } catch let error as NSError  {
+                    print("Could not save \(error), \(error.userInfo)")
+                }
+            })
+        }
+    }
+    
+    func unPark() {
+        if let selectedSpot = self.selectedSpot {
+            selectedSpot.changeStatus()
+            WebService.sharedInstance.changeStatus(selectedSpot, completionHandler: {(result) -> Void in
+
+                let coord = self.appDelegate.persistentStoreCoordinator
+                //2
+                let fetchRequest = NSFetchRequest(entityName: "UserCar")
+                let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+                
+                //4
+                do {
+                    try coord.executeRequest(deleteRequest, withContext: self.managedContext)
+                    let alert = UIAlertView(title: "Alert", message: "Done", delegate: nil, cancelButtonTitle: "Close")
+                    alert.show()
+                    self.parkUnpark.enabled = false
+                } catch let error as NSError  {
+                    print("Could not save \(error), \(error.userInfo)")
+                }
+                
+            })
+        }
+
+    }
     
     func spotTapped(sender: UITapGestureRecognizer) {
         let spot = sender.view as! Spot
@@ -183,38 +175,30 @@ class MapViewController: UIViewController, EILIndoorLocationManagerDelegate {
 //        }
     }
     
-    func updateNavigation() {
+    func updateNavigation(fromPoint: EILOrientedPoint, toPoint: EILOrientedPoint) {
         let path = UIBezierPath()
-        let spotPoint = selectedSpot!.getCoordinate()
-        let currentLocationPoint = locationView.calculatePicturePointFromRealPoint(currentPosition)
-        let point1 = locationView.calculatePicturePointFromRealPoint(closestPoint(currentPosition))
-        let point2 = locationView.calculatePicturePointFromRealPoint(closestPoint(spotPoint))
-        path.moveToPoint(currentLocationPoint)
-        let destinationPoint = locationView.calculatePicturePointFromRealPoint(spotPoint)
-        path.addLineToPoint(CGPoint(x: point1.x, y: currentLocationPoint.y))
+        let currentPoint = locationView.calculatePicturePointFromRealPoint(fromPoint)
+        let point1 = locationView.calculatePicturePointFromRealPoint(closestPoint(fromPoint))
+        let point2 = locationView.calculatePicturePointFromRealPoint(closestPoint(toPoint))
+        let destinationPoint = locationView.calculatePicturePointFromRealPoint(toPoint)
+        
+        path.moveToPoint(currentPoint)
+        path.addLineToPoint(CGPoint(x: point1.x, y: currentPoint.y))
         path.addLineToPoint(point1)
         path.addLineToPoint(CGPoint(x: point2.x, y: point1.y))
         path.addLineToPoint(point2)
         path.addLineToPoint(CGPoint(x: destinationPoint.x, y: point2.y))
         path.addLineToPoint(destinationPoint)
         
-//        path.moveToPoint(currentLocationPoint)
-//        path.addLineToPoint(CGPoint(x: currentLocationPoint.x, y: point1.y))
-//        path.addLineToPoint(point1)
-//        path.addLineToPoint(CGPoint(x: point1.x, y: point2.y))
-//        path.addLineToPoint(point2)
-//        path.addLineToPoint(CGPoint(x: point2.x, y: destinationPoint.y))
-//        path.addLineToPoint(destinationPoint)
-        
         if navigationLayer != nil {
             navigationLayer.removeFromSuperlayer()
         }
-//        navigationLayer.removeFromSuperlayer()
+
         navigationLayer = CAShapeLayer()
         navigationLayer.path = path.CGPath
         navigationLayer.strokeColor = UIColor.redColor().CGColor
         navigationLayer.fillColor = UIColor.clearColor().CGColor
-//        navigationLayer.lineWidth = 3
+        navigationLayer.lineWidth = 3
         navigationLayer.lineDashPattern = [3,6]
         navigationLayer.lineCap = kCALineCapRound
         locationView.layer.insertSublayer(navigationLayer, atIndex: 0)
@@ -232,71 +216,55 @@ class MapViewController: UIViewController, EILIndoorLocationManagerDelegate {
                 case .Medium:   accuracy = "+/- 2.62m"
                 case .Low:      accuracy = "+/- 4.24m"
                 case .VeryLow:  accuracy = "+/- ? :-("
-                default: accuracy = "dont know"
+                default: accuracy = "Something weird happended"
                 }
                 print(String(format: "x: %5.2f, y: %5.2f, orientation: %3.0f, accuracy: %@", position.x, position.y, position.orientation, accuracy))
         currentPosition = position
         self.locationView.updatePosition(position)
-        updateNavigation()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let currentPosition = self.currentPosition, selectedSpot = self.selectedSpot {
+            updateNavigation(currentPosition, toPoint: selectedSpot.getCoordinate())
+        }
     }
     
     func drawSpot(spot: Spot) {
         let identifier = "spot" + String(spot.getId())
         let point = spot.getCoordinate()
         self.locationView.drawObjectInBackground(spot, withPosition: point, identifier: identifier)
-        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("spotTapped:"))
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(MapViewController.spotTapped(_:)))
         spot.userInteractionEnabled = true
         spot.addGestureRecognizer(tapGestureRecognizer)
     }
     
     func closestPoint(point: EILOrientedPoint) -> EILOrientedPoint {
-        var resultPoint = corePoints[0]
-        for corePoint in corePoints {
+        var resultPoint = AppConstant.corePoints[0]
+        for corePoint in AppConstant.corePoints {
             if (point.distanceToPoint(corePoint) < point.distanceToPoint(resultPoint)) {
                 resultPoint = corePoint
             }
         }
         return resultPoint
     }
+    
+    func setUp() {
+        if (self.isFindingCar!) {
+            self.selectedSpot = userCar
+            if let currentPosition = self.currentPosition, selectedSpot = self.selectedSpot {
+                updateNavigation(currentPosition, toPoint: selectedSpot.getCoordinate())
+            }
+//            locationView.userInteractionEnabled = false
+        } else {
+            if let userCar = self.userCar {
+                parkUnpark.enabled = false
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
 
 
-//        Method for building the map
-//        let locationBuilder = EILLocationBuilder()
-//        locationBuilder.setLocationName("Classroom B305-ver2")
-//        locationBuilder.setLocationBoundaryPoints([
-//            EILPoint(x: 0.00, y: 0.00),
-//            EILPoint(x: 0.00, y: 9),
-//            EILPoint(x: 9, y: 9),
-//            EILPoint(x: 9, y: 0.00)])
-//
-//        locationBuilder.addBeaconIdentifiedByMac("f0d9c4f70527",
-//            atBoundarySegmentIndex: 0, inDistance: 5, fromSide: .LeftSide)
-//        locationBuilder.addBeaconIdentifiedByMac("ce59e89bd34e",
-//            atBoundarySegmentIndex: 1, inDistance: 2, fromSide: .RightSide)
-//        locationBuilder.addBeaconIdentifiedByMac("e2208d08b720",
-//            atBoundarySegmentIndex: 2, inDistance: 5, fromSide: .LeftSide)
-//        locationBuilder.addBeaconIdentifiedByMac("de0f1c1fe1e8",
-//            atBoundarySegmentIndex: 3, inDistance: 5, fromSide: .RightSide)
-//
-//        locationBuilder.setLocationOrientation(230)
-//        locationBuilder.addDoorsWithLength(2, atBoundarySegmentIndex: 0, inDistance: 1, fromSide: .RightSide)
-//        locationBuilder.addWindowWithLength(3, atBoundarySegmentIndex: 3, inDistance: 2, fromSide: .LeftSide)
-//
-//        location = locationBuilder.build()
-//        // You can get them by adding your app on https://cloud.estimote.com/#/apps
-//        ESTConfig.setupAppID("chuongtruong290893-gmail-c-af3", andAppToken: "ce9c0eea8821ff51e583c03310f7ac89")
-//        let addLocationRequest = EILRequestAddLocation(location: location)
-//        addLocationRequest.sendRequestWithCompletion { (location, error) in
-//            if error != nil {
-//                NSLog("Error when saving location: \(error)")
-//            } else {
-//                NSLog("Location saved successfully: \(location!.identifier)")
-//            }
-//        }
+
